12cf0994d8ee69fac36cf70391cc31d09e3441ac
Frederic MARTIN <frederic@martin.lc>
2013-07-02 10:19:08 +0200

    Update 30 Single storage VMs.ps1
    Speeding things up.
    Switching to layoutex container in order to avoid reporting VM with only their swap on ESX local unshared storage (but vmx/vmdk on shared ones)

--- Plugins/30 Single storage VMs.ps1
+++ Plugins/30 Single storage VMs.ps1
@@ -1,27 +1,17 @@
 # Start of Settings 
 # Local Stored VMs, do not report on any VMs who are defined here
-$LVMDoNotInclude ="Template_*|VDI*"
+$LVMDoNotInclude = "Template_*|VDI*"
 # End of Settings
 
-$Report = @()
-Foreach ($datastore in $Datastores){
-	If ($datastore.Extensiondata.summary.multiplehostaccess -eq $false){
-		ForEach ($VirtM in (get-vm -datastore $Datastore )){
-			$SAHost = "" | Select VM, Datastore
-			$SAHost.VM = $VirtM.Name 
-			$SAHost.Datastore = $Datastore.Name
-			$Report += $SAHost
-		}
-	}
-}
+$unSharedDatastore = $storageviews | ?{-Not $_.summary.multiplehostaccess} | Select Name
 
-$Result = @($Report | Where { $_.VM -notmatch $LVMDoNotInclude }) 
+$Result = $FullVM | ?{$_.Name -notmatch $LVMDoNotInclude} | ?{$_.Runtime.ConnectionState -notmatch "invalid|orphaned"} | %{$_.layoutex.file} | ?{$_.type -ne "log" -and $_.name -notmatch ".vswp$" -And $unSharedDatastore -contains $_.name.Split(']')[0].Split('[')[1]} | Select Name
 $Result
 
 $Title = "Single Storage VMs"
 $Header =  "VMs stored on non shared datastores: $(@($Result).Count)"
 $Comments = "The following VMs are located on storage which is only accesible by 1 host, these will not be compatible with VMotion and may be disconnected in the event of host failure"
 $Display = "Table"
-$Author = "Alan Renouf"
-$PluginVersion = 1.1
+$Author = "Alan Renouf, Frederic Martin"
+$PluginVersion = 1.2
 $PluginCategory = "vSphere"

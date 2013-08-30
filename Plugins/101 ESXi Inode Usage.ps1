b48b6c5f9d6bb6e5e56fbdadae45eba42a3ddc25
Matthias Koehler <matthias.koehler@stadt-frankfurt.de>
2013-08-30 12:53:29 +0200

    Added Plugin 101, ESXi filesystem Inode usage report

---
+++ Plugins/101 ESXi Inode Usage.ps1
@@ -0,0 +1,19 @@
+# Start of Settings
+# Set the ESXi filesystem free Inode threshold in percent
+$InodeThreshold = 40
+# End of Settings
+
+$Result = @($VMH | Where-Object {$_.Version -match "^5\."} | Sort-Object | % {
+	$EsxCli = Get-EsxCli -VMHost $_
+	$EsxCli.system.visorfs.get() | Where-Object {$_.FreeInodePercent -lt $InodeThreshold} | Add-Member -MemberType NoteProperty -Name VMHost -Value $_.Name -PassThru | Select-Object VMHost, FreeInodePercent, TotalInodes, UsedInodes
+})
+$Result
+
+
+$Title = "ESXi Inode Exhaustion"
+$Header = "Hosts with excessive Inode usage (> ${InodeThreshold}%) on the local ESXi visorfs: $(@($Result).count)"
+$Comments = "The following hosts have an excessive amount of Inodes in use on the local ESXi filesystem. This can cause hosts to disconnect from vCenter and becoming completely unmanageable even locally, requiring a hard reboot. See <a href="http://kb.vmware.com/kb/2037798">KB2037798</a> and <a href="http://kb.vmware.com/kb/1008643">KB1008643</a>"
+$Display = "Table"
+$Author = "Matthias Koehler"
+$PluginVersion = 1.0
+$PluginCategory = "vSphere"

70fad160f9429ce6c6f24a6754e2407b29fdb4fa
Matthias Koehler <matthias.koehler@stadt-frankfurt.de>
2013-09-02 15:43:14 +0200

    Fixed Plugin 79 Find VMs in uncontrolled snapshot mode.ps1 to handle spaces in Datastore/VM names and datacenter parentfolders. See issue #46.

--- Plugins/79 Find VMs in uncontrolled snapshot mode.ps1
+++ Plugins/79 Find VMs in uncontrolled snapshot mode.ps1
@@ -1,20 +1,28 @@
-﻿# Start of Settings
+# Start of Settings
 # End of Settings
 
 $VMFolder = @()
 foreach ($vm in $FullVM) {
       $snapList = Get-Snapshot -VM $vm.Name
       if (!$snapList) { # Only process VMs without snapshots
-            $Path = ($vm.Summary.Config.VmPathName).Split('/')[0] -replace "[\[\]]", "" -replace " ", "\"
-            $dc = Get-Datacenter -VM $vm.Name
-            $gciloc = (get-childitem vmstores: | Select -first 1).Name
-            $fileList = Get-ChildItem "vmstores:\$gciloc\$dc\$Path"
+			$vm.Summary.Config.VmPathName -match '^\[([^\]]+)\] ([^/]+)' > $null
+			$Datastore = $matches[1]
+			$VMPath = $matches[2]
+            $DC = Get-Datacenter -VM $vm.Name
+			if ($DC.ParentFolder.Parent) { #Check if Datacenter has a parent folder
+				$DCPath = $DC.ParentFolder.Name
+			}
+			else {
+				$DCPath = ''
+			}
+            $gciloc = (Get-ChildItem vmstores: | Select -first 1).Name
+            $fileList = Get-ChildItem "vmstores:\$gciloc\$DCPath\$DC\$Datastore\$VMPath"
             foreach ($file in $fileList) {
-                  if ($file -contains '-delta.vmdk' -or $file -like '-*-flat.vmdk') {
+				  if ($file.Name -like '*delta.vmdk*' -or $file -like '-*-flat.vmdk') { 
                         $Details = "" | Select-Object VM, Datacenter, Path
                         $Details.VM = $vm.Name
-                        $Details.Datacenter = $dc
-                        $Details.Path = ($vm.Summary.Config.VmPathName).Split('/')[0]
+                        $Details.Datacenter = $DC.Name
+                        $Details.Path = $Datastore + '/' + $VMPath + '/' + $file.Name
                         $VMFolder += $Details
                         break
                   }
@@ -25,8 +33,8 @@ $VMFolder
 
 $Title = "VMs in uncontrolled snapshot mode"
 $Header =  "VMs in uncontrolled snapshot mode: $(@($Result).Count)"
-$Comments = "The following VMs are in snapshot mode, but vCenter isn't aware of it. You'll need to shut down the VM and consolidate it (vmkfstools)"
+$Comments = "The following VMs are in snapshot mode, but vCenter isn't aware of it. See http://kb.vmware.com/kb/1002310"
 $Display = "Table"
-$Author = "Rick Glover"
-$PluginVersion = 1.1
+$Author = "Rick Glover, Matthias Koehler"
+$PluginVersion = 1.2
 $PluginCategory = "vSphere"

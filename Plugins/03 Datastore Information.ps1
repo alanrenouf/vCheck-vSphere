66ecf067460cfc37e38c2e35b71418911447d662
Sneddo <github@sneddo.net>
2013-09-06 16:47:54 +1000

    Added table format rules
    Added Hash table to define format rules.
    
    Key is the column name, and value is an array of rules to check. the key is the expression (with the actual cell value prepended when generating HTML), second value contains the scops of highlighting (Cell, Col, or Row), comma, then the HTML tag attribute and value.

--- Plugins/03 Datastore Information.ps1
+++ Plugins/03 Datastore Information.ps1
@@ -1,13 +1,13 @@
 # Start of Settings 
 # Set the warning threshold for Datastore % Free Space
-$DatastoreSpace =50
+$DatastoreSpace =25
 # Do not report on any Datastores that are defined here (Datastore Free Space Plugin)
 $DatastoreIgnore = "local"
 # End of Settings
 
 # ChangeLog
 # 1.2 - Added ability to filter out datastores (e.g. local ones) that you are not concerned about for the report
-
+# 1.3 - Added TableFormat for conditional formatting of output table
 if ($DatastoreIgnore){
     $OutputDatastores = @($Datastores | Where-Object {$_.Name -notmatch $DatastoreIgnore} | Select-Object Name, Type, @{N="CapacityGB";E={[math]::Round($_.CapacityGB,2)}}, @{N="FreeSpaceGB";E={[math]::Round($_.FreeSpaceGB,2)}}, PercentFree| Sort-Object PercentFree)| Where-Object { $_.PercentFree -lt $DatastoreSpace }
 }
@@ -21,6 +21,11 @@ $Title = "Datastore Information"
 $Header = "Datastores (Less than $DatastoreSpace% Free) : $(@($OutputDatastores).count)"
 $Comments = "Datastores which run out of space will cause impact on the virtual machines held on these datastores"
 $Display = "Table"
-$Author = "Alan Renouf, Jonathan Medd"
-$PluginVersion = 1.2
+$Author = "Alan Renouf, Jonathan Medd, John Sneddon"
+$PluginVersion = 1.3
 $PluginCategory = "vSphere"
+
+$TableFormat = @{"PercentFree" = @(@{ "-le 25"     = "Row,class|warning"; },
+								   @{ "-le 15"     = "Row,class|critical" });
+				 "CapacityGB"  = @(@{ "-lt 499.95" = "Cell,style|background-color: #FF0000"})			   
+				}

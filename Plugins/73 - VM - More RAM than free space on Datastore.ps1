94d07ebbb5c0e8414d42cc38c5312f1aee377305
Bob <coterl@gmail.com>
2013-10-26 18:45:01 -0400

    Updated 73 to allow for VMs stored on multiple datastores
    Previously if a VM was stored on multiple datastores the plugin would
    error out due to not being able to compare MemoryMB to a string
    (FreeSpaceMB was returned as "AAAA BBBBB", A being the size for one
    datastore B the size of the other). The plugin now finds the datastore
    the VMX file is stored on and compares the memory to that.

--- Plugins/73 - VM - More RAM than free space on Datastore.ps1
+++ Plugins/73 - VM - More RAM than free space on Datastore.ps1
@@ -1,13 +1,21 @@
 # Start of Settings 
 # End of Settings 
 
-$Result = @($VM | Where {$_.PowerState -eq "PoweredOn"} | Select Name, MemoryMB, @{N="FreeSpaceMB";E={(Get-Datastore -VM $_).FreeSpaceMB}} | Where {($_.FreeSpaceMB -ne $null) -and ($_.MemoryMB -gt $_.FreeSpaceMB)})
+$vmInfo = @()
+Foreach($Machine in $VM | Where {$_.PowerState -eq "PoweredOn"}) {
+    $Details = "" | Select Name, MemoryMB, FreeSpaceMB
+    $Details.Name = $Machine.Name
+    $Details.MemoryMB = $Machine.MemoryMB
+    $Details.FreeSpaceMB = (Get-Datastore (($Machine.ExtensionData.Config.Files.VmPathName).Split('[')[1]).Split(']')[0]).FreeSpaceMB
+    $vmInfo += $Details
+}
+$Result = @($vmInfo | Where {($_.FreeSpaceMB -ne $null) -and ($_.MemoryMB -gt $_.FreeSpaceMB)} | Sort Name)
 $Result
 
 $Title = "More RAM than free space on Datastore"
 $Header =  "More RAM than free space on Datastore: $(@($Result).Count)"
 $Comments = "The following VMs can't vMotion because they have more RAM than free space on datastore"
 $Display = "Table"
-$Author = "Olivier TABUT"
-$PluginVersion = 1.0
+$Author = "Olivier TABUT, Bob Cote"
+$PluginVersion = 1.1
 $PluginCategory = "vSphere"

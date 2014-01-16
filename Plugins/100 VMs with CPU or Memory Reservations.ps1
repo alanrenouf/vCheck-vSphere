3f7f9d2f3238118990ed6a83af61bd50279a266c
Alan Renouf <ajw.renouf@gmail.com>
2014-01-15 21:12:27 -0800

    Included filter
    Included filter as part of request in issue #78

--- Plugins/100 VMs with CPU or Memory Reservations.ps1
+++ Plugins/100 VMs with CPU or Memory Reservations.ps1
@@ -5,7 +5,9 @@ $Display = "Table"
 $Author = "Dan Jellesma"
 $PluginVersion = 1.0
 
-# Start of Settings 
+# Start of Settings
+# VMs with CPU or Memory Reservations, do not report on any VMs who are defined here
+$MCRDoNotInclude =" VM1_*|VM2_*"
 # End of Settings
-@($FullVM | ?{$_.config.cpuallocation.Reservation -ne "0" -or $_.config.memoryallocation.Reservation -ne "0"} | Select Name, @{Name="CPUReservationMhz";E={$_.config.cpuallocation.Reservation}}, @{Name="MemReservationMB";E={$_.config.memoryallocation.Reservation}})
+@($FullVM |  Where {$_.Name -notmatch $MCRDoNotInclude} | Where {$_.config.cpuallocation.Reservation -ne "0" -or $_.config.memoryallocation.Reservation -ne "0"} | Select Name, @{Name="CPUReservationMhz";E={$_.config.cpuallocation.Reservation}}, @{Name="MemReservationMB";E={$_.config.memoryallocation.Reservation}})
 $PluginCategory = "vSphere"

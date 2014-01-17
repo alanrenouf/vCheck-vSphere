eebd71e406e9f35bbdc3f3997f0be494cac8f125
Matthias Koehler <matthias.koehler@stadt-frankfurt.de>
2014-01-17 12:22:18 +0100

    Fixed indentation

--- Plugins/79 Find VMs in uncontrolled snapshot mode.ps1
+++ Plugins/79 Find VMs in uncontrolled snapshot mode.ps1
@@ -3,31 +3,31 @@
 
 $VMFolder = @()
 foreach ($vm in $FullVM) {
-	$snapList = Get-Snapshot -VM $vm.Name
-	if (!$snapList) { # Only process VMs without snapshots
-		$vm.Summary.Config.VmPathName -match '^\[([^\]]+)\] ([^/]+)' > $null
-		$Datastore = $matches[1]
-		$VMPath = $matches[2]
-		$DC = Get-Datacenter -VM $vm.Name
-		if ($DC.ParentFolder.Parent) { #Check if Datacenter has a parent folder
-			$DCPath = $DC.ParentFolder.Name
-		}
-		else {
-			$DCPath = ''
-		}
-		$gciloc = (Get-ChildItem vmstores: | Select -first 1).Name
-		$fileList = Get-ChildItem "vmstores:\$gciloc\$DCPath\$DC\$Datastore\$VMPath"
-		foreach ($file in $fileList) {
-			if ($file.Name -like '*delta.vmdk*' -or $file -like '-*-flat.vmdk') { 
-				$Details = "" | Select-Object VM, Datacenter, Path
-				$Details.VM = $vm.Name
-				$Details.Datacenter = $DC.Name
-				$Details.Path = $Datastore + '/' + $VMPath + '/' + $file.Name
-				$VMFolder += $Details
-				break
-			}
-		}
-	}
+  $snapList = Get-Snapshot -VM $vm.Name
+  if (!$snapList) { # Only process VMs without snapshots
+    $vm.Summary.Config.VmPathName -match '^\[([^\]]+)\] ([^/]+)' > $null
+    $Datastore = $matches[1]
+    $VMPath = $matches[2]
+    $DC = Get-Datacenter -VM $vm.Name
+    if ($DC.ParentFolder.Parent) { #Check if Datacenter has a parent folder
+      $DCPath = $DC.ParentFolder.Name
+    }
+    else {
+      $DCPath = ''
+    }
+    $gciloc = (Get-ChildItem vmstores: | Select -first 1).Name
+    $fileList = Get-ChildItem "vmstores:\$gciloc\$DCPath\$DC\$Datastore\$VMPath"
+    foreach ($file in $fileList) {
+      if ($file.Name -like '*delta.vmdk*' -or $file -like '-*-flat.vmdk') { 
+        $Details = "" | Select-Object VM, Datacenter, Path
+        $Details.VM = $vm.Name
+        $Details.Datacenter = $DC.Name
+        $Details.Path = $Datastore + '/' + $VMPath + '/' + $file.Name
+        $VMFolder += $Details
+        break
+      }
+    }
+  }
 }
 $VMFolder
 

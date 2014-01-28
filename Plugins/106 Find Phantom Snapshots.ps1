ca8d30e41d930dc59ee89656ab1db0d70a7821ea
Mads Fog Albrechtslund <mads@MFA-MacBook-Pro.local>
2014-01-28 22:04:54 +0100

    2 new plugins
    First time doing PowerShell/PowerCLI, so there is properly room for
    improvement.
    
    106 Find Phantom Snapshots:
    This plugin will find VM's running of Delta VMDK file, but without
    vCenter/ESXi being aware of this.
    So no snapshots reported in Snapshot Manager.
    
    107 Quantity of snapshots:
    This plugin will find VM's with more than X snapshots.

---
+++ Plugins/106 Find Phantom Snapshots.ps1
@@ -0,0 +1,41 @@
+﻿# Start of Settings 
+# End of Settings
+
+$OutputPhantomSnapshots = @()
+
+ForEach ($theVM in $VM){
+	# Inventory of VM HardDisks
+    $theVMdisks = $theVM | Get-HardDisk
+
+    
+    ForEach ($theVMdisk in $theVMdisks){
+
+    
+    	# Find VM's where active VMDK is a Delta VMDK
+    	if ($theVMdisk.Filename -like "*-??????.vmdk"){
+    		
+    		# Find VM's which don't have normal Snapshots registered 
+    		if (!(Get-Snapshot $theVM))
+{
+    			$Details = New-object PSObject
+
+	    		$Details | Add-Member -Name "VM Name" -Value $theVM.name -Membertype NoteProperty
+
+    			$Details | Add-Member -Name "VMDK Path" -Value $theVMdisk.Filename -Membertype NoteProperty
+
+    			$OutputPhantomSnapshots += $Details
+
+    		}
+        }
+    }
+}
+
+$OutputPhantomSnapshots
+
+$Title = "Find Phantom Snapshots"
+$Header =  "VM's with Phantom Snapshots : $(@($OutputPhantomSnapshots).count)"
+$Comments = "The following VM's have Phantom Snapshots"
+$Display = "Table"
+$Author = "Mads Fog Albrechtslund"
+$PluginVersion = 1.0
+$PluginCategory = "vSphere"

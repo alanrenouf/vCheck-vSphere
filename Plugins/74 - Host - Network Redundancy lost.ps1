34d0f325106f595663e9699bab3278ac2b6aeed1
kdmhorn <kenhorn68@gmail.com>
2014-03-03 11:08:49 -0500

    Update 74 - Host - Network Redundancy lost.ps1

--- Plugins/74 - Host - Network Redundancy lost.ps1
+++ Plugins/74 - Host - Network Redundancy lost.ps1
@@ -3,9 +3,9 @@
 
 $vsList = Get-VirtualSwitch
 foreach ($VMHost in $VMH) {
-   foreach($pnic in $HostsView.ExtensionData.Config.Network.Pnic){
+   foreach($pnic in $VMHost.ExtensionData.Config.Network.Pnic){
       $vSw = $vsList | where {($_.VMHost -eq $VMHost) -and ($_.Nic -contains $pNic.Device)}
-      $result = $pnic | Select @{N="ESXname";E={$HostsView.Name}},@{N="pNic";E={$pnic.Device}},@{N="vSwitch";E={$vSw.Name}},@{N="Status";E={if($pnic.LinkSpeed -ne $null){"up"}else{"down"}}}
+      $result = $pnic | Select @{N="ESXname";E={$VMHost.Name}},@{N="pNic";E={$pnic.Device}},@{N="vSwitch";E={$vSw.Name}},@{N="Status";E={if($pnic.LinkSpeed -ne $null){"up"}else{"down"}}}
       if (($result.vSwitch -ne $null) -and ($result.status -eq "down")) {$result}
    }   
 }

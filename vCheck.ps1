59b4904e60391942f1c3759745690ff765f12f4c
Sneddo <Sneddo@users.noreply.github.com>
2014-03-05 11:54:47 +1100

    Bump version number
    New version number to start version tagging. This will help us track changes between releases.

--- vCheck.ps1
+++ vCheck.ps1
@@ -30,7 +30,7 @@
 .NOTES 
    File Name  : vCheck.ps1 
    Author     : Alan Renouf - @alanrenouf
-   Version    : 6.19
+   Version    : 6.20
    
    Thanks to all who have commented on my blog to help improve this project
    all beta testers and previous contributors to this script.
@@ -61,7 +61,7 @@ param (
    [ValidateScript({Test-Path $_ -PathType 'Leaf'})]
    [string]$job
 )
-$Version = "6.19"
+$Version = "6.20"
 
 ################################################################################
 #                                  Functions                                   #

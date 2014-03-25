b684ece9a7332b393169b2aa2528f13f9971fb71
Sneddo <Sneddo@users.noreply.github.com>
2014-03-25 11:40:54 +1100

    Create 117 Hosts with Alarm disabled.ps1

---
+++ Plugins/117 Hosts with Alarm disabled.ps1
@@ -0,0 +1,20 @@
+# Start of Settings
+$IncludeMaintenance = $false
+# End of Settings
+
+$AlarmActionsEnabled =	$VMH | Where { (-not $_.ExtensionData.AlarmActionsEnabled)} | 
+									Select @{Name="Host"; Expression={$_.Name}}, @{"Name"="InMaintenanceMode";Expression={$_.ExtensionData.Runtime.InMaintenanceMode}}, @{"Name"="AlarmActionsEnabled"; Expression={$_.ExtensionData.AlarmActionsEnabled}} |
+									Sort-Object Host
+
+if ($IncludeMaintenance -eq $false) {
+	$AlarmActionsEnabled = $AlarmActionsEnabled | Where {-not $_.InMaintenanceMode}
+}
+$AlarmActionsEnabled
+
+$Title = "Hosts with Alarm disabled"
+$Header =  "Hosts with Alarms disabled : $(@($AlarmActionsEnabled).Count)"
+$Comments = "The following Hosts have Alarm disabled. This may impact the Alarming of your infrastrucure."
+$Display = "Table"
+$Author = "Denis Gauthier"
+$PluginVersion = 1.1
+$PluginCategory = "vSphere"

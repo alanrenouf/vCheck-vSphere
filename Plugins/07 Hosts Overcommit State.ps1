9e9d2c5c23df0bb91ad2056163d1a0113e71f0d8
Sneddo <Sneddo@users.noreply.github.com>
2014-03-31 14:28:05 +1100

    Added support for MB and GB units
    Pull request #178 - simplified code.

--- Plugins/07 Hosts Overcommit State.ps1
+++ Plugins/07 Hosts Overcommit State.ps1
@@ -1,31 +1,44 @@
-# Start of Settings 
-# End of Settings 
+# Start of Settings
+# Return results in GB or MB?
+$Units ="GB"
+# End of Settings
 
-$MyObj = @()
+$OverCommit = @()
+
 Foreach ($VMHost in $VMH) {
-	$Details = "" | Select Host, TotalMemMB, TotalAssignedMemMB, TotalUsedMB, OverCommitMB
-	$Details.Host = $VMHost.Name
-	$Details.TotalMemMB = $VMHost.MemoryTotalMB
 	if ($VMMem) { Clear-Variable VMMem }
-   $VM | ?{$_.Host.Name -eq $VMHost.Name} | Foreach {
+	$VM | ?{$_.VMHost.Name -eq $VMHost.Name} | Foreach {
 		[INT]$VMMem += $_.MemoryMB
 	}
-	$Details.TotalAssignedMemMB = $VMMem
-	$Details.TotalUsedMB = $VMHost.MemoryUsageMB
-	If ($Details.TotalAssignedMemMB -gt $VMHost.MemoryTotalMB) {
-		$Details.OverCommitMB = ($Details.TotalAssignedMemMB - $VMHost.MemoryTotalMB)
-	} Else {
-		$Details.OverCommitMB = 0
+
+	If ([Math]::Round(($VMMem - $VMHost.MemoryTotalMB), 0) -gt 0) {
+		$OverCommitMB = [Math]::Round(($VMMem - $VMHost.MemoryTotalMB), 0)
+
+		if ($Units -eq "MB") {
+			$OverCommit += New-Object PSObject -Property @{"Host" = $VMHost.Name;
+								"TotalMemMB" = [Math]::Round($VMHost.MemoryTotalMB,0);
+								"TotalAssignedMemMB" = $VMMem;
+								"TotalUsedMB" = [Math]::Round($VMHost.MemoryUsageMB,0);
+								"OverCommitMB" = $OverCommitMB;
+																		}
+		}
+		else {
+			$OverCommit += New-Object PSObject -Property @{"Host" = $VMHost.Name;
+								"TotalMemGB" = [Math]::Round(($VMHost.MemoryTotalMB)/1024,0);
+								"TotalAssignedMemGB" = [Math]::Round($VMMem/1024,0);
+								"TotalUsedGB" = [Math]::Round(($VMHost.MemoryUsageMB)/1024,0);
+								"OverCommitGB" = [Math]::Round($OverCommitMB/1024, 0);
+																		}
+		}
 	}
-	$MyObj += $Details
 }
-$OverCommit = @($MyObj | Where {$_.OverCommitMB -gt 0})
-$OverCommit
+
+$OverCommit | Select Host, "TotalMem$Units", "TotalAssignedMem$Units", "TotalUsed$Units", "OverCommit$Units"
 
 $Title = "Hosts Overcommit state"
 $Header =  "Hosts overcommitting memory : $(@($OverCommit).count)"
 $Comments = "Overcommitted hosts may cause issues with performance if memory is not issued when needed, this may cause ballooning and swapping"
 $Display = "Table"
 $Author = "Alan Renouf"
-$PluginVersion = 1.2
+$PluginVersion = 1.3
 $PluginCategory = "vSphere"

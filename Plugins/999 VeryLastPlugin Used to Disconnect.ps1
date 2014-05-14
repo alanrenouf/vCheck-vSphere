fa7e58dcf96f90b8f24dfb0940dcfc63fe89c3e6
Sneddo <github@sneddo.net>
2014-05-14 11:12:55 +1000

    Added Remove-variable to hopefully combat high memory usage due to data collection objects lingering when launching interactively

--- Plugins/999 VeryLastPlugin Used to Disconnect.ps1
+++ Plugins/999 VeryLastPlugin Used to Disconnect.ps1
@@ -6,10 +6,14 @@ If ($VIConnection) {
   $VIConnection | Disconnect-VIServer -Confirm:$false
 }
 
+# Unset variables
+$vars = @("VM", "VMH", "Clusters", "Datastores", "FullVM", "VMTmpl", "ServiceInstance", "alarmMgr", "HostsViews", "clusviews", "storageviews", "DatastoreClustersView")
+$vars | foreach { Remove-Variable $_ }
+
 $Title = "Disconnecting from vCenter"
 $Display = "None"
 $Author = "Alan Renouf"
-$PluginVersion = 1.1
+$PluginVersion = 1.2
 $Header =  "Disconnects from vCenter"
 $Comments = "Disconnect plugin"
 $Display = "None"

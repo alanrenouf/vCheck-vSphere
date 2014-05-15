ed6cc0c9952ff08e15feaebe4e2a841ef8d79b16
Petar Enchev <haripetrov@gmail.com>
2014-05-15 10:46:42 +0300

    Update 66 Misnamed VM.ps1

--- Plugins/66 Misnamed VM.ps1
+++ Plugins/66 Misnamed VM.ps1
@@ -1,7 +1,7 @@
 # Start of Settings
 # Misnamed VMs, do not report on any VMs who are defined here
-$MNDoNotInclude =" VM1_*|VM2_*"
-# End of Settings 
+$MNDoNotInclude = "VM1_*|VM2_*"
+# End of Settings
 
 $misnamed = @()
 foreach ($vmguest in ($FullVM | Where {$_.Name -notmatch $MNDoNotInclude} | where { $_.Guest.HostName -ne $NULL -AND $_.Guest.HostName -notmatch $_.Name })) {
@@ -14,7 +14,7 @@ foreach ($vmguest in ($FullVM | Where {$_.Name -notmatch $MNDoNotInclude} | wher
 $misnamed
 
 $Title = "Mis-named virtual machines"
-$Header =  "Mis-named virtual machines"
+$Header = "Mis-named virtual machines"
 $Comments = "The following guest names do not match the name inside of the guest."
 $Display = "Table"
 $Author = "Frederic Martin"

9638fb45c742cbff5efc65eb5344989259d3d2b2
Petar Enchev <haripetrov@gmail.com>
2014-05-15 10:14:17 +0300

    Update 45 VMs needing snapshot consolidation.ps1

--- Plugins/45 VMs needing snapshot consolidation.ps1
+++ Plugins/45 VMs needing snapshot consolidation.ps1
@@ -7,7 +7,7 @@ $Consol = $FullVM | ?{$htabHostVersion[$_.runtime.host].Split('.')[0] -ge 5 -and
 $Consol
 
 $Title = "VMs needing snapshot consolidation"
-$Header =  "VMs needing snapshot consolidation $(@($Consol).Count)"
+$Header = "VMs needing snapshot consolidation $(@($Consol).Count)"
 $Comments = "The following VMs have snapshots that failed to consolidate. See <a href='http://blogs.vmware.com/vsphere/2011/08/consolidate-snapshots.html' target='_blank'>this article</a> for more details"
 $Display = "Table"
 $Author = "Luc Dekens, Frederic Martin"

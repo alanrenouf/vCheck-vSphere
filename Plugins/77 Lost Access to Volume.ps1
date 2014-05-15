1f80cbfc0e4a3d6fd4c32f786dce439b74c6c23c
Petar Enchev <haripetrov@gmail.com>
2014-05-15 10:58:33 +0300

    Update and rename 77 Lost access to volume.ps1 to 77 Lost Access to Volume.ps1

---
+++ Plugins/77 Lost Access to Volume.ps1
@@ -0,0 +1,18 @@
+# Start of Settings
+# Set the number of days of Lost Action Volume to report and count on
+$LostAccessVolumeAge = 1
+# End of Settings
+
+# Changelog
+## 1.1 : Correctly formatted the Start / End Settings and used $MaxSampleVIEvent which is defined in plugin 00
+
+$Result = @(Get-VIEvent -Start ($Date).AddDays(-$LostAccessVolumeAge) -MaxSamples $MaxSampleVIEvent | Where-Object {$_.GetType().Name -eq "EventEx" -and $_.EventTypeId -like "esx.problem.vmfs.heartbeat.*"} | Select-Object -Property @{Name="VMHost";Expression={$_.Host.Name}},CreatedTime,FullFormattedMessage | Sort-Object -Property VMHost,CreatedTime)
+$Result
+
+$Title = "Lost Access to Volume"
+$Header = "Lost Access to Volume: $(@($Result | Where-Object {$_.FullFormattedMessage -like "Lost access to volume *"}).Count)"
+$Comments = "The following hosts have lost access to a volume. This may indicate a problem with your storage solution."
+$Display = "Table"
+$Author = "Robert van den Nieuwendijk, Jonathan Medd"
+$PluginVersion = 1.1
+$PluginCategory = "vSphere"

348dd0e2c5f576364085f7ba6691cdb97df1d4aa
Petar Enchev <haripetrov@gmail.com>
2014-05-15 10:45:52 +0300

    Update and rename 65 Snapshot created.ps1 to 65 Snapshot Created.ps1

---
+++ Plugins/65 Snapshot Created.ps1
@@ -0,0 +1,21 @@
+# Start of Settings 
+# Set the number of days to show Snapshots created for
+$VMsNewRemovedAge = 5
+# User exception for Snapshot created
+$snapshotUserException = "s-veeam"
+# End of Settings
+
+$EventFilterSpec = New-Object VMware.Vim.EventFilterSpec
+$EventFilterSpec.Category = "info"
+$EventFilterSpec.Time = New-Object VMware.Vim.EventFilterSpecByTime
+$EventFilterSpec.Time.beginTime = ((get-date).adddays(-$VMsNewRemovedAge)).ToUniversalTime()
+$EventFilterSpec.eventTypeId = "TaskEvent"
+(get-view (get-view ServiceInstance -Property Content.EventManager).Content.EventManager).QueryEvents($EventFilterSpec) | ?{$_.FullFormattedMessage -match "Create virtual machine snapshot" -and $_.userName -notmatch $snapshotUserException} | Select @{N="Created Time";E={($_.createdTime).ToLocalTime()}}, @{N="User";E={$_.userName}}, @{N="VM Name";E={$_.vm.name}}
+
+$Title = "Snapshot created"
+$Header = "Snapshot created (Last $VMsNewRemovedAge Day(s)) (with user exception $snapshotUserException)"
+$Comments = ""
+$Display = "Table"
+$Author = "Raphael Schitz, Frederic Martin"
+$PluginVersion = 1.4
+$PluginCategory = "vSphere"

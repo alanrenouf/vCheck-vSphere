40b0ac096992345725caa608b5099fd239e77069
Petar Enchev <haripetrov@gmail.com>
2014-05-15 10:56:05 +0300

    Update and rename 75 Hosts with different hostnames.ps1 to 75 Hosts with Different Hostnames.ps1

--- Plugins/75 Hosts with Different Hostnames.ps1
+++ Plugins/75 Hosts with Different Hostnames.ps1
@@ -1,11 +1,8 @@
-﻿# Start of Settings
+# Start of Settings
 # End of Settings
 
 $report = @($VMH | Where-Object {$_.Name.Split('.')[0] -ne $_.NetworkInfo.HostName} | Select-Object -Property Name,@{Name="HostName";Expression={$_.NetworkInfo.HostName}} | Sort-Object -Property Name)
-
 $report
-
-　
 
 $Title = "Hosts with different hostname"
 $Header = "Hosts with different hostname: $($Report.Count)"
@@ -13,4 +10,4 @@ $Comments = "The following hosts have a different hostname than their name in th
 $Display = "Table"
 $Author = "Robert van den Nieuwendijk"
 $PluginVersion = 1.0
-$PluginCategory = "vSphere" 
+$PluginCategory = "vSphere"

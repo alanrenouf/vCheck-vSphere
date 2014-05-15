1713daea7f5f74c205ec6bd64965e63c72369a70
Petar Enchev <haripetrov@gmail.com>
2014-05-15 11:40:53 +0300

    Update 116 sDRS VM Behavior not Default.ps1

--- Plugins/116 sDRS VM Behavior not Default.ps1
+++ Plugins/116 sDRS VM Behavior not Default.ps1
@@ -6,18 +6,14 @@
 ## 1.0 : Initial Version
 
 
-
-
 $Result = @($DatastoreClustersView | Foreach {$dc = $_;$dc.PodStorageDrsEntry.StorageDrsConfig.VMConfig} | `
 	Where {$_.Enabled -eq $false -or $_.Behavior -ne $null} | `
 	Select @{N="VM";E={Get-View $_.Vm | Select -ExpandProperty Name}}, Enabled, Behavior,@{N="Datastore Cluster";E={$dc.Name}})
-
-
 $Result
 
 
 $Title = "sDRS VM Behavior not Default"
-$Header =  "VMs overriding Datastore Cluster automation level: $(@($Result).Count)"
+$Header = "VMs overriding Datastore Cluster automation level: $(@($Result).Count)"
 $Comments = "The following VMs are overriding the Datastore Cluster sDRS automation level"
 $Display = "Table"
 $Author = "Shawn Masterson"

8eceb68ad8c0a788b9016999969d3d3e71ead535
Petar Enchev <haripetrov@gmail.com>
2014-05-15 11:23:20 +0300

    Update 104 Clusters with no Host Profile.ps1

--- Plugins/104 Clusters with no Host Profile.ps1
+++ Plugins/104 Clusters with no Host Profile.ps1
@@ -7,7 +7,7 @@ $HostProfiles = Get-VMHostProfile | Select Name, @{Name="ClusterID";Expression={
 $clusviews | ?{($HostProfiles | Select -expandProperty ClusterID) -notcontains $_.moref } | Sort-Object Name | Select Name
 
 $Title = "Clusters Without Host Profile attached"
-$Header =  "Clusters Without Host Profile attached"
+$Header = "Clusters Without Host Profile attached"
 $Comments = "The following clusters do not have a host profile attached"
 $Display = "Table"
 $Author = "John Sneddon"

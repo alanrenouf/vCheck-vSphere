1c62c9677be94e442f72fe2871287aec816fbf54
Sneddo <Sneddo@users.noreply.github.com>
2014-08-07 09:12:38 +1000

    Update from Issue #235
    New code from Issue #235. May still be some events that are not returned. Failed (s)vMotions will not be listed.

--- Plugins/20 Cluster/72 s-vMotion Information.ps1
+++ Plugins/20 Cluster/72 s-vMotion Information.ps1
@@ -3,26 +3,27 @@
 $vMotionAge = 14
 # End of Settings
 
-Function Get-MotionDuration {
-    $events = Get-VIEvent -Start ((Get-Date).AddDays(-$vMotionAge)) -MaxSamples $MaxSampleVIEvent
-    $relocates = $events |
-        where {$_.GetType().Name -eq "TaskEvent" -and $_.Info.DescriptionId -eq "VirtualMachine.migrate" -or $_.Info.DescriptionId -eq "VirtualMachine.relocate"}
-    foreach($task in $relocates){
-        $tEvents = $events | where {$_.ChainId -eq $task.ChainId} |
-            Sort-Object -Property CreatedTime
-        if($tEvents.Count){
-            New-Object PSObject -Property @{
-                Name = $tEvents[0].Vm.Name
-                Type = &{if($tEvents[0].Host.Name -eq $tEvents[-1].Host.Name){"SvMotion"}else{"vMotion"}}
-                StartTime = $tEvents[0].CreatedTime
-                EndTime = $tEvents[-1].CreatedTime
-                Duration = New-TimeSpan -Start $tEvents[0].CreatedTime -End $tEvents[-1].CreatedTime
+# Search for any vmotion-related events
+$EventFilterSpec = New-Object VMware.Vim.EventFilterSpec
+$EventFilterSpec.Category = "info"
+$EventFilterSpec.Time = New-Object VMware.Vim.EventFilterSpecByTime
+$EventFilterSpec.Time.beginTime = (get-date).adddays(-$vMotionAge)
+$EventFilterSpec.Type = "VmMigratedEvent", "DrsVmMigratedEvent", "VmBeingHotMigratedEvent", "VmBeingMigratedEvent"
+$vmotions = @((get-view (get-view ServiceInstance -Property Content.EventManager).Content.EventManager).QueryEvents($EventFilterSpec)) 
+
+$Motions = @()
+# Group by chainID - each chain should have a start and finish event
+foreach($vmotion in ($vmotions | Sort-object CreatedTime | Group-Object ChainID)) {
+    if($vmotion.Group.count -eq 2){
+            $Motions += New-Object PSObject -Property @{
+                Name = $vmotion.Group[0].vm.name
+                Type = &{if($vmotion.Group[0].Host.Name -eq $vmotion.Group[1].Host.Name){"SvMotion"}else{"vMotion"}}
+                StartTime = $vmotion.Group[0].CreatedTime
+                EndTime = $vmotion.Group[1].CreatedTime
+                Duration = New-TimeSpan -Start $vmotion.Group[0].CreatedTime -End $vmotion.Group[1].CreatedTime
             }
-        }
     }
 }
-
-$Motions = Get-MotionDuration
 $Motions
 
 $Title = "s/vMotion Information"
@@ -30,5 +31,7 @@ $Header = "s/vMotion Information (Over $vMotionAge Days Old) : $(@($Motions).cou
 $Comments = "s/vMotions and how long they took to migrate between hosts and datastores"
 $Display = "Table"
 $Author = "Alan Renouf"
-$PluginVersion = 1.0
+$PluginVersion = 1.1
 $PluginCategory = "vSphere"
+
+Remove-Variable Motions, EventFilterSpec, vmotions

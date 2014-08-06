f428f6b06c2627beb4e7ddaa4d05be81aac4a114
Sneddo <Sneddo@users.noreply.github.com>
2014-08-07 09:11:24 +1000

    Update from Issue #236
    New code to move away from limitations of Get-VIEvent

--- Plugins/20 Cluster/15 HA VMs restarted.ps1
+++ Plugins/20 Cluster/15 HA VMs restarted.ps1
@@ -1,20 +1,23 @@
 # Start of Settings 
 # HA VM restart day(s) number
-$HAVMrestartold =5
+$HAVMrestartold = 5
 # End of Settings
 
 $EventFilterSpec = New-Object VMware.Vim.EventFilterSpec
-$EventFilterSpec.Category = "info"
+$EventFilterSpec.Category = "warning"
+$EventFilterSpec.eventTypeId = "com.vmware.vc.ha.VmRestartedByHAEvent"
 $EventFilterSpec.Time = New-Object VMware.Vim.EventFilterSpecByTime
-$EventFilterSpec.Time.beginTime = (get-date).adddays(-$HAVMrestartold)
-$EventFilterSpec.eventTypeId = "TaskEvent"
-$HAVMrestartlist = @((get-view (get-view ServiceInstance -Property Content.EventManager).Content.EventManager).QueryEvents($EventFilterSpec) | ?{$_.FullFormattedMessage -match "was restarted"} |select CreatedTime,FullFormattedMessage |sort CreatedTime -Descending)
+$EventFilterSpec.Time.beginTime = (Get-Date).AddDays(-$HAVMrestartold)
+
+$HAVMrestartlist = @((get-view (get-view ServiceInstance -Property Content.EventManager).Content.EventManager).QueryEvents($EventFilterSpec) | select CreatedTime,FullFormattedMessage |sort CreatedTime -Descending)
 $HAVMrestartlist
 
 $Title = "HA VMs restarted"
-$Header = "HA: VM restart (Last $HAVMrestartold Day(s)) : $(@($HAVMrestartlist).count)"
+$Header = ("HA: VM restart (Last {0} Day(s)) : {1}" -f $HAVMrestartold, @($HAVMrestartlist).count)
 $Comments = "The following VMs have been restarted by HA in the last $HAVMresetold days"
 $Display = "Table"
 $Author = "Alan Renouf"
 $PluginVersion = 1.1
 $PluginCategory = "vSphere"
+
+Remove-Variable HAVMrestartlist, EventFilterSpec

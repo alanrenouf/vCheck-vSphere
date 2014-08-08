b30c4d98ab58e705de51afc3dad469afeefb6427
Frederic MARTIN <frederic@martin.lc>
2014-08-08 11:46:14 +0200

    Fixing $NULL false evaluation
    $NULL wasn't well evaluated, switching to "" instead

--- Plugins/60 VM/66 Misnamed VM.ps1
+++ Plugins/60 VM/66 Misnamed VM.ps1
@@ -4,7 +4,7 @@ $MNDoNotInclude = "VM1_*|VM2_*"
 # End of Settings
 
 $misnamed = @()
-foreach ($vmguest in ($FullVM | Where {$_.Name -notmatch $MNDoNotInclude} | where { $_.Guest.HostName -ne $NULL -AND $_.Guest.HostName -notmatch $_.Name })) {
+foreach ($vmguest in ($FullVM | Where {$_.Name -notmatch $MNDoNotInclude -AND $_.Guest.HostName -ne "" -AND $_.Guest.HostName -notmatch $_.Name })) {
 	$myObj = "" | select VMName,GuestName
 	$myObj.VMName = $vmguest.name
 	$myObj.GuestName = $vmguest.Guest.HostName
@@ -18,5 +18,5 @@ $Header = "Mis-named virtual machines"
 $Comments = "The following guest names do not match the name inside of the guest."
 $Display = "Table"
 $Author = "Frederic Martin"
-$PluginVersion = 1.1
+$PluginVersion = 1.2
 $PluginCategory = "vSphere"

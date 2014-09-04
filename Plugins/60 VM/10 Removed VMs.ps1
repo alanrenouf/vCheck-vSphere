0c9cf88e120ab9c718da9ad45a474232fd223d66
Nebojsa Ilic <github@nilic.33mail.com>
2014-09-04 14:17:21 +0200

    Fixes #245
    Fix for plugins 09, 10 and 78

--- Plugins/60 VM/10 Removed VMs.ps1
+++ Plugins/60 VM/10 Removed VMs.ps1
@@ -1,14 +1,14 @@
 # Start of Settings 
 # Set the number of days to show VMs removed for
-$VMsNewRemovedAge =5
+$VMsNewRemovedAge = 5
 # End of Settings
 
-$OutputRemovedVMs = @($VIEvent | where {$_.Gettype().Name -eq "VmRemovedEvent"}| Select CreatedTime, UserName, fullFormattedMessage)
+$OutputRemovedVMs = @(Get-VIEventPlus -Start ((get-date).adddays(-$VMsNewRemovedAge)) -EventType "VmRemovedEvent" | Select CreatedTime, UserName, fullFormattedMessage)
 $OutputRemovedVMs
 
 $Title = "Removed VMs"
-$Header = "VMs Removed (Last $VMsNewRemovedAge Day(s)) : $(@($OutputRemovedVMs).count)"
-$Comments = "The following VMs have been removed/deleted over the last $($VMsNewRemovedAge) days"
+$Header = ("VMs Removed (Last {0} Day(s)): {1}" -f $VMsNewRemovedAge,$OutputRemovedVMs.count)
+$Comments = ("The following VMs have been removed/deleted over the last {0} days" -f $VMsNewRemovedAge)
 $Display = "Table"
 $Author = "Alan Renouf"
 $PluginVersion = 1.1

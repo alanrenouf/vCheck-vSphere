db1b76f0b7e9d657b2d586059f26c6dd6a1dfd3e
Sneddo <Sneddo@users.noreply.github.com>
2014-09-09 13:01:53 +1000

    Fix #245
    Fix for string formatting error

--- Plugins/60 VM/09 Created or cloned VMs.ps1
+++ Plugins/60 VM/09 Created or cloned VMs.ps1
@@ -6,7 +6,7 @@ $VMsNewRemovedAge =5
 @(Get-VIEventPlus -Start ((get-date).adddays(-$VMsNewRemovedAge)) -EventType @("VmCreatedEvent", "VmBeingClonedEvent", "VmBeingDeployedEvent") | Select createdTime, UserName, fullFormattedMessage)
 
 $Title = "Created or cloned VMs"
-$Header =  ("VMs Created or Cloned (Last {0} Day(s)): {count)" -f $VMsNewRemovedAge)
+$Header =  ("VMs Created or Cloned (Last {0} Day(s)): [count])" -f $VMsNewRemovedAge)
 $Comments = ("The following VMs have been created over the last {0} Days" -f $VMsNewRemovedAge)
 $Display = "Table"
 $Author = "Alan Renouf"

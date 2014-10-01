6c8558fd9b936d750f03e7ddd5f3bee7fcdb750d
sybux <sybux@users.noreply.github.com>
2014-10-01 16:12:34 +0200

    Create 620 CBT Status.ps1
    List all VMs with CBT status disabled

---
+++ Plugins/60 VM/620 CBT Status.ps1
@@ -0,0 +1,15 @@
+# Start of Settings 
+# End of Settings
+
+$CBTEnabled = $false
+
+$VMsCBTStatus = @($FullVm | Where-object {$_.Config.ChangeTrackingEnabled -eq $CBTEnabled} | Select-Object Name, @{Name="Change Block Tracking";Expression={if ($_.Config.ChangeTrackingEnabled) { "enabled" } else { "disabled" }}} | Sort Name)
+$VMsCBTStatus
+
+$Title = "VM - Display all VMs with CBT not enabled"
+$Header = "VM with CBT disabled : $(@($VMsCBTStatus).Count)"
+$Comments = "List all VMs with CBT status disabled. It's not a good option for backup !"
+$Display = "Table"
+$Author = "Cyril Epiney"
+$PluginVersion = 1.0
+$PluginCategory = "vSphere"

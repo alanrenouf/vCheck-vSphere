16438312dd5a2961d90c052893683cabeb8d8c71
Chris Monahan <monahancj@users.noreply.github.com>
2014-11-04 14:52:57 -0500

    Update 25 VMs in inconsistent folders.ps1
    We have a storage appliance that has a high turnover of VMs and we don't want to sVMotion the VMs off of the appliance to fix the folder naming.  Adding the filter removes a couple dozen VMs from the report that we don't need to see.

--- Plugins/60 VM/25 VMs in inconsistent folders.ps1
+++ Plugins/60 VM/25 VMs in inconsistent folders.ps1
@@ -1,4 +1,5 @@
-# Start of Settings 
+# Start of Settings
+$DatastoreIgnore = "local"
 # End of Settings 
 
 $VMFolder = @()
@@ -6,7 +7,7 @@ Foreach ($CHKVM in $FullVM){
 	$Details = "" |Select-Object VM,Path
 	$Folder = ((($CHKVM.Summary.Config.VmPathName).Split(']')[1]).Split('/'))[0].TrimStart(' ')
 	$Path = ($CHKVM.Summary.Config.VmPathName).Split('/')[0]
-	If ($CHKVM.Name-ne $Folder){
+	If (($CHKVM.Name-ne $Folder) -and ($Path -notmatch $DatastoreIgnore)){
 		$Details.VM= $CHKVM.Name
 		$Details.Path= $Path
 		$VMFolder += $Details}
@@ -14,9 +15,10 @@ Foreach ($CHKVM in $FullVM){
 $VMFolder
 
 $Title = "VMs in inconsistent folders"
-$Header = "VMs in Inconsistent folders $(@($VMFolder).Count)"
+$Header = "VMs in Inconsistent folders $(@($VMFolder).Count), excluding those on datastores @($DatastoreIgnore)"
 $Comments = "The following VMs are not stored in folders consistent to their names, this may cause issues when trying to locate them from the datastore manually"
 $Display = "Table"
 $Author = "Alan Renouf"
-$PluginVersion = 1.1
+$PluginVersion = 1.2
 $PluginCategory = "vSphere"
+# Datastore filtering added by monahancj

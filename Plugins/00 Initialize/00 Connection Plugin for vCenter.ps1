f479fa5f82e80e144b4b4df50e2c672798210bc1
Sneddo <Sneddo@users.noreply.github.com>
2014-11-07 16:03:15 +1100

    Push plugin default strings into plugin file
    Makes more sense to define strings in the plugin, and only override when another language is present.

--- Plugins/00 Initialize/00 Connection Plugin for vCenter.ps1
+++ Plugins/00 Initialize/00 Connection Plugin for vCenter.ps1
@@ -11,6 +11,30 @@ $PluginCategory = "vSphere"
 $MaxSampleVIEvent = 100000
 # End of Settings
 
+# Setup plugin-specific language table
+$pLang = DATA {
+   ConvertFrom-StringData @' 
+      connReuse = Re-using connection to VI Server
+      connOpen  = Connecting to VI Server
+      connError = Unable to connect to vCenter, please ensure you have altered the vCenter server address correctly. To specify a username and password edit the connection string in the file $GlobalVariables
+      custAttr  = Adding Custom properties
+      collectVM = Collecting VM Objects
+      collectHost = Collecting VM Host Objects
+      collectCluster = Collecting Cluster Objects
+      collectDatastore = Collecting Datastore Objects
+      collectDVM = Collecting Detailed VM Objects
+      collectTemplate = Collecting Template Objects
+      collectDVIO = Collecting Detailed VI Objects
+      collectAlarm = Collecting Detailed Alarm Objects
+      collectDHost = Collecting Detailed VMHost Objects
+      collectDCluster = Collecting Detailed Cluster Objects
+      collectDDatastore = Collecting Detailed Datastore Objects
+      collectDDatastoreCluster = Collecting Detailed Datastore Cluster Objects
+'@
+}
+# Override the default (en) if it exists in lang directory
+Import-LocalizedData -BaseDirectory ($ScriptPath + "\lang") -BindingVariable pLang -ErrorAction SilentlyContinue
+
 # Find the VI Server and port from the global settings file
 $VIServer = ($Server -Split ":")[0]
 if (($server -split ":")[1]) {

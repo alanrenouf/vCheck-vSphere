110500a06c1d3d925548657411a40ea6fc1a069f
Petar Enchev <haripetrov@gmail.com>
2014-11-11 13:48:13 +0200

    Update "48 Find VM Disk Format"

--- Plugins/60 VM/48 Find VM Disk Format.ps1
+++ Plugins/60 VM/48 Find VM Disk Format.ps1
@@ -1,9 +1,11 @@
 # Start of Settings 
 # Report on disk formats that are not "thin" or "thick", which format is not allowed?
 $diskformat = "thick"
+# Specify Datastores to filter from report
+$DatastoreIgnore = "local"
 # End of Settings
 
-$vmdiskformat = $VM | Get-HardDisk | where {$_.storageformat -match $diskformat} | select @{N="VM";E={$_.parent.name}}, @{N="DiskName";E={$_.name}}, @{N="Format";E={$_.storageformat}}, @{N="FileName";E={$_.filename}}
+$vmdiskformat = $VM | Get-HardDisk | where {($_.storageformat -match $diskformat) -and ($_.Filename -notmatch $DatastoreIgnore)} | select @{N="VM";E={$_.parent.name}}, @{N="DiskName";E={$_.name}}, @{N="Format";E={$_.storageformat}}, @{N="FileName";E={$_.filename}}
 $vmdiskformat
 
 $Title = "Find VMs with thick or thin provisioned vmdk"

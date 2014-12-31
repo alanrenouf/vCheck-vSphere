4eda81e231a40e7b13e550349001452c29d604a5
rvdnieuwendijk <robert@van-den-nieuwendijk.nl>
2014-12-31 09:46:34 +0100

    Update 58 Virtual machines with less hard disks than partitions.ps1
    Added a check to prevent VMs managed by SRM to show in the list, because the SRM replica VMs have no hard disks connected.

--- Plugins/60 VM/58 Virtual machines with less hard disks than partitions.ps1
+++ Plugins/60 VM/58 Virtual machines with less hard disks than partitions.ps1
@@ -1,8 +1,9 @@
 # Start of Settings
 # End of Settings
 
-$Result = @($FullVM | `
-	Select-Object -Property Name,@{N="NrOfHardDisks";E={($_.Layout.Disk|measure).count}},@{N="NrOfGuestDisks";E={($_.Guest.Disk|measure).count}},@{N="GuestFamily";E={$_.Guest.GuestFamily}}| `
+$Result = @($FullVM |
+	Where-Object {$_.Config.ManagedBy.ExtensionKey -ne 'com.vmware.vcDr'} |
+	Select-Object -Property Name,@{N="NrOfHardDisks";E={($_.Layout.Disk|measure).count}},@{N="NrOfGuestDisks";E={($_.Guest.Disk|measure).count}},@{N="GuestFamily";E={$_.Guest.GuestFamily}} |
 	Where-Object {$_.GuestFamily -eq "windowsGuest" -and $_.NrOfHardDisks -lt $_.NrOfGuestDisks}
  )
 $Result
@@ -12,5 +13,5 @@ $Header = "Virtual machines with less hard disks than partitions : $(@($Result).
 $Comments = "Virtual machines with less hard disks than partitions. Probably they have more than one partition on a hard disk."
 $Display = "Table"
 $Author = "Robert van den Nieuwendijk"
-$PluginVersion = 1.1
+$PluginVersion = 1.2
 $PluginCategory = "vSphere"

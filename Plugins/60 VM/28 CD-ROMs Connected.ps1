79266363f27c542726e51de2227ea754a1e52fa4
rvdnieuwendijk <robert@van-den-nieuwendijk.nl>
2015-01-12 11:13:59 +0100

    Update 28 CD-ROMs Connected.ps1
    Added the Label and Summary properties to the output, to show the device name of the connected CD-ROM device and the path to the connected ISO file or the connected CD-ROM.

--- Plugins/60 VM/28 CD-ROMs Connected.ps1
+++ Plugins/60 VM/28 CD-ROMs Connected.ps1
@@ -3,7 +3,12 @@
 $CDFloppyConnectedOK = "APP*|ETC*"
 # End of Settings
 
-$Result = @($FullVM | ?{$_.runtime.powerState -eq "PoweredOn" -And $_.Name -notmatch $CDFloppyConnectedOK} | ?{$_.config.hardware.device | ?{$_ -is [VMware.Vim.VirtualCdrom] -And $_.connectable.connected}} | Select Name)
+$Result = @($FullVM |
+  Where-Object {$_.runtime.powerState -eq "PoweredOn" -and $_.Name -notmatch $CDFloppyConnectedOK} |
+  Where-Object {$_.config.hardware.device | Where-Object {$_ -is [VMware.Vim.VirtualCdrom] -and $_.connectable.connected}} |
+  Select-Object -Property Name,
+    @{Name='Label';Expression={($_.config.hardware.device | Where-Object {$_ -is [VMware.Vim.VirtualCdrom] -and $_.connectable.connected}).DeviceInfo.Label}},
+    @{Name='Summary';Expression={($_.config.hardware.device | Where-Object {$_ -is [VMware.Vim.VirtualCdrom] -and $_.connectable.connected}).DeviceInfo.Summary}})
 $Result
 
 $Title = "CD-ROMs Connected"
@@ -11,5 +16,5 @@ $Header = "VM: CD-ROM Connected - vMotion Violation: $(@($Result).Count)"
 $Comments = "The following VMs have a CD-ROM connected, this may cause issues if this machine needs to be migrated to a different host"
 $Display = "Table"
 $Author = "Alan Renouf, Frederic Martin"
-$PluginVersion = 1.2
+$PluginVersion = 1.3
 $PluginCategory = "vSphere"

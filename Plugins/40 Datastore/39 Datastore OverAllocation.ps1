a605da1798b76494acfdf9a45bf1baa7c7ecb558
Greg Hatch <greg.hatch@raymondjames.com>
2015-09-09 16:01:51 -0400

    Updated Comment to be more clear
    Updated Comment in "39 Datastore OverAllocation" to be more clear

--- Plugins/40 Datastore/39 Datastore OverAllocation.ps1
+++ Plugins/40 Datastore/39 Datastore OverAllocation.ps1
@@ -25,9 +25,9 @@ $voverallocation
 
 $Title = "Datastore OverAllocation"
 $Header = "Datastore OverAllocation Over $OverAllocation%: $(@($voverallocation).Count)"
-$Comments = "The following datastores may be overcommitted, it is strongly suggested you check these"
+$Comments = "The following datastores may be overcommitted (over-provisioned) and could run out of space. It is strongly suggested you check these below. You can start an sVMotion to see the Capacity vs. Provisioned space for datastores."
 $Display = "Table"
 $Author = "Alan Renouf"
-$PluginVersion = 1.3
+$PluginVersion = 1.4
 $PluginCategory = "vSphere"
 # 20150223 monahancj - Added datastore filtering

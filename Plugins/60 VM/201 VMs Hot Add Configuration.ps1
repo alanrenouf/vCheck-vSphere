a5497e37d1ec3b9241b16614854cf4b4aaee2f11
Sneddo <Sneddo@users.noreply.github.com>
2016-08-11 15:16:40 +1000

    Add option to report on disabled or enabled
    Plugin can now be used to report if CPU/MEM hot plug is enabled or disabled.

--- Plugins/60 VM/201 VMs Hot Add Configuration.ps1
+++ Plugins/60 VM/201 VMs Hot Add Configuration.ps1
@@ -1,14 +1,21 @@
 # Start of Settings 
+# Should CPU hot plug be enabled
+$CPUHotAdd = $true
+# Should Memory hot add be enabled
+$MEMHotAdd = $true
 # End of Settings
 
-$HotAdd = @($VM | Select-Object Name, @{N="CPU Hot Plug Enabled"; E={$_.ExtensionData.config.CpuHotAddEnabled}}, @{N="Memory Hot Add Enabled"; E={$_.ExtensionData.config.MemoryHotAddEnabled}})
-$HotAdd
+@($VMs | Select-Object Name, @{N="CPU Hot Plug Enabled"; E={$_.ExtensionData.config.CpuHotAddEnabled}}, @{N="Memory Hot Add Enabled"; E={$_.ExtensionData.config.MemoryHotAddEnabled}}) | Where {$_."CPU Hot Plug Enabled" -ne $CPUHotAdd -or $_."Memory Hot Add Enabled" -ne $MEMHotAdd}
+
+# Create variables with unexpected values, for use in the plugin comment
+$CPUNotExpected = if ($CPUHotAdd) { "disabled" } else { "enabled" }
+$MEMNotExpected = if ($MEMHotAdd) { "disabled" } else { "enabled" }
 
 $Title = "VMs Memory/CPU Hot Add configuration"
 $Header = "VMs Memory/CPU Hot Add configuration"
-$Comments = "The following lists all VMs and they Hot Add / Hot Plug feature configuration"
+$Comments = ("The following lists all VMs with CPU hot plug {0} or Memory hot add {1}" -f $CPUNotExpected, $MEMNotExpected)
 
 $Display = "Table"
 $Author = "Marc Bouchard"
-$PluginVersion = 1.0
+$PluginVersion = 1.1
 $PluginCategory = "vSphere"

245f1dbec07969115dc44134eef7b7360d652277
Sneddo <Sneddo@users.noreply.github.com>
2016-08-15 10:53:27 +1000

    Update to use Get-VIEventPlus function
    Previously this plugin was rolling its own search.
    
    Issue #477

--- Plugins/20 Cluster/15 HA VMs restarted.ps1
+++ Plugins/20 Cluster/15 HA VMs restarted.ps1
@@ -1,23 +1,14 @@
-# Start of Settings 
+# Start of Settings
 # HA VM restart day(s) number
 $HAVMrestartold = 5
 # End of Settings
 
-$EventFilterSpec = New-Object VMware.Vim.EventFilterSpec
-$EventFilterSpec.Category = "warning"
-$EventFilterSpec.eventTypeId = "com.vmware.vc.ha.VmRestartedByHAEvent"
-$EventFilterSpec.Time = New-Object VMware.Vim.EventFilterSpecByTime
-$EventFilterSpec.Time.beginTime = (Get-Date).AddDays(-$HAVMrestartold)
-
-$HAVMrestartlist = @((get-view (get-view ServiceInstance -Property Content.EventManager).Content.EventManager).QueryEvents($EventFilterSpec) | select CreatedTime,FullFormattedMessage |sort CreatedTime -Descending)
-$HAVMrestartlist
+@(Get-VIEventPlus -EventType "com.vmware.vc.ha.VmRestartedByHAEvent" -Start (Get-Date).AddDays(-$HAVMrestartold) | Select-Object CreatedTime, FullFormattedMessage | Sort-Object CreatedTime -Descending)
 
 $Title = "HA VMs restarted"
-$Header = ("HA: VM restart (Last {0} Day(s)) : {1}" -f $HAVMrestartold, @($HAVMrestartlist).count)
+$Header = ("HA: VM restart (Last {0} Day(s)) : [count]" -f $HAVMrestartold)
 $Comments = "The following VMs have been restarted by HA in the last $HAVMresetold days"
 $Display = "Table"
 $Author = "Alan Renouf"
-$PluginVersion = 1.1
+$PluginVersion = 1.2
 $PluginCategory = "vSphere"
-
-Remove-Variable HAVMrestartlist, EventFilterSpec

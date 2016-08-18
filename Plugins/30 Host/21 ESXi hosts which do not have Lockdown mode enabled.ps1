10b02fd84b9316f65b11461da9917ea683b6e4c2
Sneddo <Sneddo@users.noreply.github.com>
2016-08-18 15:00:18 +1000

    Slight code cleanup

--- Plugins/30 Host/21 ESXi hosts which do not have Lockdown mode enabled.ps1
+++ Plugins/30 Host/21 ESXi hosts which do not have Lockdown mode enabled.ps1
@@ -1,12 +1,13 @@
 # Start of Settings 
 # End of Settings 
 
-$ESXiLockDown = $VMH | Where {$_.ConnectionState -eq "Connected" -or $_.ConnectionState -eq "Maintenance"} | Where {$_.ExtensionData.Summary.Config.Product.Name -match "i"} | Select Name, @{N="LockedMode";E={$_.ExtensionData.Config.AdminDisabled}}
-$Result = @($ESXiLockDown | Where { $_.LockedMode -eq $false })
-$Result
+$VMH | Where {@("Connected","Maintenance") -contains $_.ConnectionState -and 
+              $_.ExtensionData.Summary.Config.Product.Name -match "i" -and 
+              -not $_.ExtensionData.Config.AdminDisabled} | `
+                 Select Name, @{N="LockedMode";E={$_.ExtensionData.Config.AdminDisabled}}
 
 $Title = "ESXi hosts which do not have Lockdown mode enabled"
-$Header = "ESXi Hosts with Lockdown Mode not Enabled : $(@($Result).count)"
+$Header = "ESXi Hosts with Lockdown Mode not Enabled: [count]"
 $Comments = "The following ESXi Hosts do not have lockdown enabled, think about using lockdown as an extra security feature."
 $Display = "Table"
 $Author = "Alan Renouf"

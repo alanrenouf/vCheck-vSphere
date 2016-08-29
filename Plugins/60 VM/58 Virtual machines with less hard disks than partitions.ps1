175aedfcfedffc34ae734b9b1085152028af5ce3
Sneddo <Sneddo@users.noreply.github.com>
2016-08-29 14:05:45 +1000

    Add VM filter
    Add standard VM filter

--- Plugins/60 VM/58 Virtual machines with less hard disks than partitions.ps1
+++ Plugins/60 VM/58 Virtual machines with less hard disks than partitions.ps1
@@ -1,7 +1,9 @@
 # Start of Settings
+# Do not report on any VMs who are defined here (regex)
+$VMTDoNotInclude = "VM1_*|VM2_*"
 # End of Settings
 
-$Result = @($FullVM |
+$Result = @($FullVM | Where {$_.Name -notmatch $VMTDoNotInclude} |
 	Where-Object {$_.Config.ManagedBy.ExtensionKey -ne 'com.vmware.vcDr'} |
 	Select-Object -Property Name,@{N="NrOfHardDisks";E={($_.Layout.Disk|measure).count}},@{N="NrOfGuestDisks";E={($_.Guest.Disk|measure).count}},@{N="GuestFamily";E={$_.Guest.GuestFamily}} |
 	Where-Object {$_.GuestFamily -eq "windowsGuest" -and $_.NrOfHardDisks -lt $_.NrOfGuestDisks}
@@ -13,5 +15,5 @@ $Header = "Virtual machines with less hard disks than partitions : $(@($Result).
 $Comments = "Virtual machines with less hard disks than partitions. Probably they have more than one partition on a hard disk."
 $Display = "Table"
 $Author = "Robert van den Nieuwendijk"
-$PluginVersion = 1.2
+$PluginVersion = 1.3
 $PluginCategory = "vSphere"

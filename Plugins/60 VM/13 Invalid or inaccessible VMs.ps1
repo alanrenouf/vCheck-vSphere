63e251ccd372dbb96374cf4d94029bf8f41c678c
Sneddo <Sneddo@users.noreply.github.com>
2016-10-03 07:36:31 +1100

    Removed Stray ]

--- Plugins/60 VM/13 Invalid or inaccessible VMs.ps1
+++ Plugins/60 VM/13 Invalid or inaccessible VMs.ps1
@@ -5,7 +5,7 @@ $FullVM | Where {$_.Runtime.ConnectionState -eq "invalid" -or $_.Runtime.Connect
     Select Name, @{Name="ConnectionState";e={$_.Runtime.ConnectionState}}, @{Name="PowerState";e={$_.Runtime.PowerState}}, @{Name="IP_Address";e={$_.Guest.IpAddress}}
 
 $Title = "Invalid or inaccessible VM"
-$Header = "VM invalid or inaccessible: [count]]"
+$Header = "VM invalid or inaccessible: [count]"
 $Comments = "The following VMs are marked as inaccessible or invalid"
 $Display = "Table"
 $Author = "Alan Renouf"

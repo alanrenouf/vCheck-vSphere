ac29e0010229c4d061811198e91bc93ecb6645ba
Sneddo <Sneddo@users.noreply.github.com>
2016-10-10 15:44:59 +1100

    Update to fix #478
    Made sure $fileList is an array, so adding to it won't throw an error.
    
    Updated to use Write-Progress to provide feedback on long-running plugin.

--- Plugins/60 VM/79 Find VMs in Uncontrolled Snapshot Mode.ps1
+++ Plugins/60 VM/79 Find VMs in Uncontrolled Snapshot Mode.ps1
@@ -1,33 +1,52 @@
-# Start of Settings
-# End of Settings
-
-$VMFolder = @()
-foreach ($eachDS in $Datastores) {
-    $FilePath = $eachDS.DatastoreBrowserPath + '\*\*delta.vmdk*'
-    $fileList = Get-ChildItem -Path "$FilePath" | Select Name, FolderPath, FullName
-    $FilePath = $eachDS.DatastoreBrowserPath + '\*\-*-flat.vmdk'
-    $fileList += Get-ChildItem -Path "$FilePath" | Select Name, FolderPath, FullName
-
-    foreach ($vmFile in $filelist | sort FolderPath) {
-        $vmFile.FolderPath -match '^\[([^\]]+)\] ([^/]+)' > $null
-        $VMName = $matches[2]
-        $eachVM = $FullVM | where {$_.Name -eq $VMName}
-        if (!$eachVM.snapshot) { # Only process VMs without snapshots
-            $Details = "" | Select-Object VM, Datacenter, Path
-            $Details.VM = $eachVM.Name
-            $Details.Datacenter = $eachDS.Datacenter
-            $Details.Path = $vmFile.FullName
-            $VMFolder += $Details
-        }
-    }
-}
-$Results = $VMFolder | sort VM
-$Results
-
 $Title = "VMs in uncontrolled snapshot mode"
-$Header = "VMs in uncontrolled snapshot mode: $(@($Result).Count)"
+$Header = "VMs in uncontrolled snapshot mode: [count]"
 $Comments = "The following VMs are in snapshot mode, but vCenter isn't aware of it. See http://kb.vmware.com/kb/1002310"
 $Display = "Table"
 $Author = "Rick Glover, Matthias Koehler, Dan Rowe"
-$PluginVersion = 1.4
+$PluginVersion = 1.5
 $PluginCategory = "vSphere"
+
+# Start of Settings
+# End of Settings
+
+# Setup plugin-specific language table
+$pLang = DATA {
+   ConvertFrom-StringData @' 
+      pluginActivity = Searching Datastores
+      pluginStatus = [{0} of {1}] {2}
+      Complete = Complete
+'@
+}
+# Override the default (en) if it exists in lang directory
+Import-LocalizedData -BaseDirectory ($ScriptPath + "\lang") -BindingVariable pLang -ErrorAction SilentlyContinue
+
+$VMFolder = @()
+$i=0;
+foreach ($eachDS in ($Datastores | Where {$_.Accessible})) {
+   Write-Progress -ID 2 -Parent 1 -Activity $pLang.pluginActivity -Status ($pLang.pluginStatus -f $i, $Datastores.count, $eachDS.Name) -PercentComplete ($i*100/$Datastores.count)
+   $eachDS.Name
+   $FilePath = $eachDS.DatastoreBrowserPath + '\*\*delta.vmdk*'
+   $fileList = @(Get-ChildItem -Path "$FilePath" | Select Name, FolderPath, FullName)
+   $FilePath = $eachDS.DatastoreBrowserPath + '\*\-*-flat.vmdk'
+   $fileList += Get-ChildItem -Path "$FilePath" | Select Name, FolderPath, FullName
+
+   $i++
+   
+   foreach ($vmFile in $filelist | sort FolderPath) 
+   {
+      $vmFile.FolderPath -match '^\[([^\]]+)\] ([^/]+)' > $null
+      $VMName = $matches[2]
+      $eachVM = $FullVM | where {$_.Name -eq $VMName}
+      if (!$eachVM.snapshot) 
+      { 
+         # Only process VMs without snapshots
+         $Details = "" | Select-Object VM, Datacenter, Path
+         $Details.VM = $eachVM.Name
+         $Details.Datacenter = $eachDS.Datacenter
+         $Details.Path = $vmFile.FullName
+         $VMFolder += $Details
+      }
+   }
+}
+Write-Progress -ID 1 -Activity $pLang.pluginActivity -Status $pLang.Complete -Completed
+$VMFolder | sort VM

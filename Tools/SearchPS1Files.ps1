c0bbfd14c1b59304989620ed82c6a7fd70a69b54
lucdekens <dekens.luc@gmail.com>
2017-09-20 14:17:21 +0200

    Delete SearchPS1Files.ps1

--- Tools/SearchPS1Files.ps1
+++
@@ -1,8 +0,0 @@
-﻿$rootFolder = 'D:\Temp\vCheck-vSphere'
-$search = 'Windowscreds.xml'
-
-foreach($file in (Get-ChildItem -Path $rootFolder -Filter *.ps1 -Recurse -File)){
-    Get-Content -Path $file.FullName | where{$_ -match [Regex]::Escape($search)} |
-    Select @{N='File';E={$file.FullName}},
-        @{N='Line';E={$_}}
-}

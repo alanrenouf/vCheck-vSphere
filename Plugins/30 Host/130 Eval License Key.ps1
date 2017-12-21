5b67b2245fce0481c33f248034c06e511090c335
Sneddo <Sneddo@users.noreply.github.com>
2017-12-22 10:24:42 +1100

    Simplify code

--- Plugins/30 Host/130 Eval License Key.ps1
+++ Plugins/30 Host/130 Eval License Key.ps1
@@ -1,4 +1,5 @@
 $Title = "Eval License Keys"
+$Header = "Hosts using an Evaluation Key : [count]"
 $Comments = "The following hosts are using an evaluation key that will expire, causing them to disconnect from vCenter."
 $Display = "Table"
 $Author = "Doug Taliaferro"
@@ -8,15 +9,4 @@ $PluginCategory = "vSphere"
 # Start of Settings
 # End of Settings
 
-$hostResults = @()
-foreach ($esxhost in ($VMH | where {$_.ConnectionState -match "Connected|Maintenance"})) {
-    if ($esxhost.LicenseKey -eq "00000-00000-00000-00000-00000") {
-        $myObj = "" | Select VMHost, LicenseKey
-        $myObj.VMHost = $esxhost.Name
-        $myObj.LicenseKey = $esxhost.LicenseKey
-        $hostResults += $myObj
-	}
-}
-$hostResults
-
-$Header = "Hosts using an Evaluation Key : [count]"
+$VMH | where-object {($_.ConnectionState -match "Connected|Maintenance") -and ($_.LicenseKey -eq "00000-00000-00000-00000-00000")} | Select Name, LicenseKey

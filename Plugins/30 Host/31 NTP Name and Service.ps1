67353c72c86cf013c35ea8ae1f03c3d0a000e91c
Bill Wall <thebillness@users.noreply.github.com>
2017-12-27 14:39:03 -0600

    Updated version info and change log

--- Plugins/30 Host/31 NTP Name and Service.ps1
+++ Plugins/30 Host/31 NTP Name and Service.ps1
@@ -2,8 +2,8 @@ $Title = "NTP Name and Service"
 $Header = "NTP Issues: [count]"
 $Comments = "The following hosts do not have the correct NTP settings and may cause issues if the time becomes far apart from the vCenter/Domain or other hosts"
 $Display = "Table"
-$Author = "Alan Renouf, Dan Barr"
-$PluginVersion = 1.3
+$Author = "Alan Renouf, Dan Barr, Bill Wall"
+$PluginVersion = 1.4
 $PluginCategory = "vSphere"
 
 # Start of Settings
@@ -15,3 +15,4 @@ $VMH | Where-Object {$_.ConnectionState -match "Connected|Maintenance"} | Select
 
 # Changelog
 ## 1.3 : Only check Connected hosts since Disconnected and Not Responding produce empty data
+## 1.4 : Added check for Maintenance mode VMHs

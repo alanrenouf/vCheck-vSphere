7057c5a796cb5978c1ec2101f28bcfd26377b113
Sneddo <Sneddo@users.noreply.github.com>
2018-01-30 09:58:52 +1100

    Missing underscore
    Issue #621

--- Plugins/30 Host/74 Host - Network Redundancy lost.ps1
+++ Plugins/30 Host/74 Host - Network Redundancy lost.ps1
@@ -13,7 +13,7 @@ $vsList = Get-VirtualSwitch
 foreach ($VMHost in $VMH) {
    foreach($pnic in $VMHost.ExtensionData.Config.Network.Pnic){
       $vSw = $vsList | Where-Object {($_.VMHost -eq $VMHost) -and ($_.Nic -contains $pNic.Device)}
-      $pnic | Select-Object @{N="ESXname";E={$VMHost.Name}},@{N="pNic";E={$pnic.Device}},@{N="vSwitch";E={$vSw.Name}},@{N="Status";E={if($pnic.LinkSpeed -ne $null){"up"}else{"down"}}} | Where-Object {($_.Status -eq "down") -and ($.vSwitch -notlike $null)}
+      $pnic | Select-Object @{N="ESXname";E={$VMHost.Name}},@{N="pNic";E={$pnic.Device}},@{N="vSwitch";E={$vSw.Name}},@{N="Status";E={if($pnic.LinkSpeed -ne $null){"up"}else{"down"}}} | Where-Object {($_.Status -eq "down") -and ($_.vSwitch -notlike $null)}
    }   
 }
 

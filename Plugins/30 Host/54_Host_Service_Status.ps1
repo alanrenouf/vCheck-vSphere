c3c6341854241499096c4cdb2edeba13b2d77656
F <58272808+ascii42@users.noreply.github.com>
2020-01-25 21:12:23 +0100

    Add files via upload

---
+++ Plugins/30 Host/54_Host_Service_Status.ps1
@@ -0,0 +1,13 @@
+$Title = "Host Service Status"
+Get-VMHost | Get-VMHostService | Where-Object {($_.Required -match "True" -and $_.Running -notmatch "True") -or ($_.Required -match "False" -and $_.Running -match "True" -and $_.Key -notmatch "vpxa|DCUI|TSM-SSH|lbtd|ntpd|sfcbd-watchdog|TSM|snmpd|vmware-fdm")} | Select VMHost,Key,Label,Policy,Running,Required 
+$Header = "Host Service Status"
+# End of Settings
+$Title = "Host Service Status"
+$Header = "Host Service Status"
+$Comments = "Check if the Hosts Services works as expected"
+$Display = "Table"
+$Author = "Felix Longardt"
+$PluginVersion = 0.1
+$PluginCategory = "vSphere"
+
+Get-VMHost | Get-VMHostService | Where-Object {($_.Required -match "True" -and $_.Running -notmatch "True") -or ($_.Required -match "False" -and $_.Running -match "True" -and $_.Key -notmatch "vpxa|DCUI|TSM-SSH|lbtd|ntpd|sfcbd-watchdog|TSM|snmpd|vmware-fdm")} | Select VMHost,Key,Label,Policy,Running,Required 

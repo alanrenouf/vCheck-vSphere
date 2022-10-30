6d48a0b28ead23ef93e616fa0fa65fd652e9b1fc
Jonathan Colon <jcolonf@zenprsolutions.com>
2022-10-29 23:06:35 -0400

    Added Changelog Plugin 200 - VMs on Ephemeral Portgroup not showing results #750

--- Plugins/60 VM/200 VMs on ephemeral portgroup.ps1
+++ Plugins/60 VM/200 VMs on ephemeral portgroup.ps1
@@ -3,7 +3,7 @@ $Header = "VMs on Ephemeral Portgroup: [count]"
 $Comments = ""
 $Display = "Table"
 $Author = "Tim Williams"
-$PluginVersion = 1.0
+$PluginVersion = 1.1
 $PluginCategory = "vSphere"
 
 # Start of Settings
@@ -11,3 +11,7 @@ $PluginCategory = "vSphere"
 
 $EphemeralPG = Get-VDSwitch | Get-VDPortgroup | Where-Object {$_.PortBinding -eq "Ephemeral"}
 $VM | Get-NetworkAdapter | Where-Object {$_.NetworkName -in $EphemeralPG.Name} | Select-Object @{Name="VMName"; Expression={$_.parent}}, @{Name="Portgroup"; Expression={$_.NetworkName}}
+
+# Change Log
+## 1.0 : Initial release
+## 1.1 : Modified Where-Object filter to retreive result when there are more then one $EphemeralPG object

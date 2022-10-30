5923d21af33e88e92dd688b51b033eafa0282d95
Jonathan Colon <jcolonf@zenprsolutions.com>
2022-10-29 22:57:37 -0400

    Resolve #750

--- Plugins/60 VM/200 VMs on ephemeral portgroup.ps1
+++ Plugins/60 VM/200 VMs on ephemeral portgroup.ps1
@@ -8,6 +8,6 @@ $PluginCategory = "vSphere"
 
 # Start of Settings
 # End of Settings
- 
+
 $EphemeralPG = Get-VDSwitch | Get-VDPortgroup | Where-Object {$_.PortBinding -eq "Ephemeral"}
-$VM | Get-NetworkAdapter | Where-Object {$_.NetworkName -contains $EphemeralPG} | Select-Object @{Name="VMName"; Expression={$_.parent}}, @{Name="Portgroup"; Expression={$_.NetworkName}}
+$VM | Get-NetworkAdapter | Where-Object {$_.NetworkName -in $EphemeralPG.Name} | Select-Object @{Name="VMName"; Expression={$_.parent}}, @{Name="Portgroup"; Expression={$_.NetworkName}}

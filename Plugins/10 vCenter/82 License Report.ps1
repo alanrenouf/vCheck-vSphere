3111caaf8418179f3dc5b255c8e4684897617dd6
Jonathan Colon <jcolonf@zenprsolutions.com>
2022-10-30 13:14:19 -0400

    Resolve #744

--- Plugins/10 vCenter/82 License Report.ps1
+++ Plugins/10 vCenter/82 License Report.ps1
@@ -3,14 +3,31 @@
 $licenseEvals = $true
 # End of Settings
 
-$vSphereLicInfo = @()
-
-Foreach ($LicenseMan in Get-View ($ServiceInstance | Select-Object -First 1).Content.LicenseManager) 
+Foreach ($LicenseMan in Get-View ($ServiceInstance | Select-Object -First 1).Content.LicenseManager)
 {
-   ($LicenseMan | Select-Object -ExpandProperty Licenses) | Where {$licenseEvals -or $_.Name -notmatch "Evaluation" } | Select-Object  @{Name="VC";e={([Uri]$LicenseMan.Client.ServiceUrl).Host}}, `
-      Name, LicenseKey, Total, Used, @{Name="Information";e={$_.Labels | Select-Object -ExpandProperty Value}}, `
-      @{"Name"="ExpirationDate";e={$_.Properties | Where-Object { $_.key -eq "expirationDate" } | Select-Object -ExpandProperty Value}} 
+    $vSphereLicInfo = @()
+    foreach ($License in ($LicenseMan | Select-Object -ExpandProperty Licenses | Where-Object {$licenseEvals -or $_.Name -notmatch "Evaluation" })) {
+        $ExpirationDate = $License.Properties | Where-Object { $_.key -eq "expirationDate"} | Select-Object -ExpandProperty Value
+        $inObj = [ordered] @{
+            'vCenter Server' = ([URI]$LicenseMan.Client.ServiceUrl).Host
+            'Product' = $License.Name
+            'License Key' = $License.LicenseKey
+            'Capacity' = $License.Total
+            'Usage' = $License.Used
+            'Information' = $License.Labels
+            'Expiration Date' = Switch ([string]::IsNullOrEmpty($ExpirationDate)) {
+                $true {'Never'}
+                $false {$ExpirationDate.ToShortDateString()}
+                default {'Unknown'}
+            }
+        }
+
+        $vSphereLicInfo += [pscustomobject]$inobj
+
+    }
 }
+
+$vSphereLicInfo
 
 $Title = "vCenter License Report"
 $Header = "License Report"
@@ -24,3 +41,4 @@ $PluginCategory = "vSphere"
 ## 1.0 : Initial Release
 ## 1.1 : Code refactor
 ## 1.2 : Added the ability to exclude evaluation licenses, clean up whitespace (@thebillness)
+## 1.3 : Added the ability to detect never expiring licenses
